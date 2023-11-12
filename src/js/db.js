aac89a5a3de11dba127905724c4e80351306d2e0
TXTuser <gimosteg666@gmail.com>
2023-11-12 18:15:53 +0300

    #style-done

--- src/js/db.js
+++ src/js/db.js
@@ -43,6 +43,45 @@ let db = [
     master.`,
     museum: "http://kosmo-museum.ru",
   },
+
+  {
+    img: "../src/img/Ship of Fools.png",
+    name: "Ship of Fools",
+    artist: "Hieronymus Bosch",
+    description: `Painting “Ship of Fools”. Why ship? A common metaphor in Bosch's time. This is what they said about the Church. She must “carry” her parishioners through worldly vanity to spiritual purity.
+    But something is wrong with Bosch's ship. Its passengers indulge in empty fun. They are bawling and drinking. Both monks and laymen. They don’t even notice that their ship is no longer sailing anywhere. And so long ago that a tree grew through the bottom.
+    Pay attention to the jester. A fool by profession behaves more seriously than others. He turned away from those having fun and drinks his compote. Without him, there are already enough fools on this ship.
+    “Ship of Fools” is the upper part of the right wing of the triptych. The lower one is stored in another country. On it we see the shore. The bathers threw off their clothes and surrounded the barrel of wine.
+    Two of them swam to the ship of fools. Look, one of them has the same bowl as the bather next to the barrel.`,
+    museum: "http://kosmo-museum.ru",
+  },
+  {
+    img: "../src/img/Temptation of Saint Anthony.png",
+    name: "Temptation of Saint Anthony",
+    artist: "Hieronymus Bosch",
+    description: `Temptation of Saint Anthony. Another fantastic triptych by Bosch. Among the heap of monsters and behemoths are four stories from the life of a hermit.
+    First, the saint in heaven is tormented by demons. Satan sent them. He was haunted by the fact that he was struggling with earthly temptations.
+    The demons threw the exhausted saint to the ground. We see him, exhausted, being led by the arms.
+    In the central part, the saint is already kneeling among mysterious characters. It is the alchemists who are trying to seduce him with the elixir of eternal life. As we know, nothing worked out for them.`,
+    museum: "http://kosmo-museum.ru",
+  },
+  {
+    img: "../src/img/Prodigal son.png",
+    name: "Prodigal son",
+    artist: "Hieronymus Bosch",
+    description: `In the painting “Prodigal Son”, instead of a huge number of characters, there is one main character. Traveler.
+    He is pretty battered by life. But he has hope. Leaving the world of depravity and sin, he wants to return home to his father. To the world of righteous life and spiritual grace.
+    He looks back at the house, which is an allegory for a dissolute lifestyle. Tavern or inn. A temporary shelter full of primitive amusements.
+    The roof is leaky. The shutter is warped. A visitor relieves himself just around the corner. And two of them are having fun in the doorway. All this symbolizes spiritual degradation.`,
+    museum: "http://kosmo-museum.ru",
+  },
+  {
+    img: "../src/img/Wanderer.png",
+    name: "Wanderer",
+    artist: "Hieronymus Bosch",
+    description: `The meaning here is similar. We are travelers. There is much to be happy about along our journey. But there are also many dangers. Where are we going? And will we get somewhere? Or will we wander like this until death overtakes us on the road?`,
+    museum: "http://kosmo-museum.ru",
+  },
 ];
 
 export { db };
--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,21 @@ let parent = document.getElementsByClassName("container")[0];
 
 for (let i = 0; i < db.length; i++) {
   let card = `
-    <div class="card">
-    <img src="${db[i].img}" alt="">
-    <div class="info">
-        <p class="name">${db[i].name}</p>
-        <p class="artist">${db[i].artist}</p>
-        <p class="description">${db[i].description}</p>
-        <a href="${db[i].museum}" target="_blank">Museum</a>
-        <a href="#/">More</a>
-    </div>
+  <div class="card">
+  <img src="${db[i].img}" alt="">
+  <div class="info">
+      <div class="info__top-side">
+          <p class="name">${db[i].name}</p>
+          <p class="artist">${db[i].artist}</p>
+      </div>
+      <div class="info_bottom-side">
+          <p class="description">${db[i].description}</p>
+      <div class="info_bottom-menu"> 
+          <a href="${db[i].museum}" target="_blank">Museum</a>
+          <a href="#/">More</a>
+      </div>
+      </div>
+  </div>
 </div>
     `;
   parent.innerHTML = parent.innerHTML + card;
